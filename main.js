172e3f427d0213239ce14270fe1d39b70a1a3f61
transferitore <93931008+transferitore@users.noreply.github.com>
2022-07-01 16:05:14 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
-serve((_req) => {
-  return new Response("Hello World!", {
-    headers: { "content-type": "text/plain" },
+const HTML = await Deno.readFile("./index.html");
+
+serve(async () => {
+  return new Response(HTML, {
+    headers: {
+      "content-type": "text/html",
+    },
   });
 });
