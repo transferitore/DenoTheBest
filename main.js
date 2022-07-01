1492e90c05c6adb9746f9ca0c5704ef3c25c4665
transferitore <93931008+transferitore@users.noreply.github.com>
2022-07-01 14:03:52 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
-import { Application } from "https://deno.land/x/abc@v1.0.0-rc10/mod.ts";
+import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
-const app = new Application();
-
-app
-  .get("/hello", (c) => {
-    return "Hello World"
-  })
-  .static("/", "./public")
-  .start();
+serve((_req) => {
+  return new Response("Hello World!", {
+    headers: { "content-type": "text/plain" },
+  });
+});
